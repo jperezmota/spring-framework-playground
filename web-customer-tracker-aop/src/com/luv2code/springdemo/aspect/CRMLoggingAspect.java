fb5e887bf5d2e16b5a4b48983e90f4ed302d2c34
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-08 02:29:25 -0400

    Fixing pointcut typo. Adding After aspect method.

--- web-customer-tracker-aop/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package com.luv2code.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -14,20 +16,35 @@ public class CRMLoggingAspect {
 	Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// setup point cut declaration
-	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*.(..))")
+	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
 	public void forControllerPackage() {}
 	
-	@Pointcut("execution(* com.luv2code.springdemo.service.*.*.(..))")
+	@Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
 	public void forServicePackage() {}
 	
-	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*.(..))")
+	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
 	public void forDaoPackage() {}
 	
 	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
 	public void forAppFlow() {}
 	
-	
 	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// display method we are calling.
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("===>> in @Before: calling method: " + theMethod);
+		
+		// display the arguments to the method
+		Object[] args = theJoinPoint.getArgs();
+		
+		// loop through and display the arguments
+		for(Object tempArg: args) {
+			myLogger.info("=====>> argument: " + tempArg);
+		}
+	}
+	
 	
 	// add @AfterReturning advice
 
