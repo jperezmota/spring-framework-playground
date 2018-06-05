5375cbd59d1871b81bae1df559e0214ffbabf262
Jonathan Perez <jonathanperezmota@gmail.com>
2018-06-05 15:11:21 -0400

    Reading JSON file and binding it to a POJO

--- jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -1,5 +1,9 @@
 package com.luv2code.jackson.json.demo;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Driver {
 
 	public static void main(String[] args) {
@@ -7,10 +11,20 @@ public class Driver {
 		
 		try {
 			
+			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
 			
+			// read JSON file and map/convert to JAVA POJO
+			// data/simple-lite.json
+			
+			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			
+			// print first name and last name
+			System.out.println("First Name: " + theStudent.getFirstName());
+			System.out.println("Last Name: " + theStudent.getLastName());
 			
 		}catch (Exception exc){
-			
+			exc.printStackTrace();
 		}
 
 	}
--- jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -10,6 +10,38 @@ public class Student {
 	public Student() {
 		
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 	
 	
 
