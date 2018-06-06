3e83a0bc140e081e849ac0743c0d34fe9264f278
Jonathan Perez <jonathanperezmota@gmail.com>
2018-06-06 12:36:35 -0400

    Adding Address class

--- jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -10,18 +10,27 @@ public class Driver {
 		
 		
 		try {
-			
 			// create object mapper
 			ObjectMapper mapper = new ObjectMapper();
 			
 			// read JSON file and map/convert to JAVA POJO
 			// data/simple-lite.json
 			
-			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			// print first name and last name
 			System.out.println("First Name: " + theStudent.getFirstName());
 			System.out.println("Last Name: " + theStudent.getLastName());
+			
+			//print out address: street and city
+			Address tempAddress = theStudent.getAddress();
+			System.out.println("Street: " + theStudent.getAddress().getStreet());
+			System.out.println("City: " + theStudent.getAddress().getCity());
+			
+			// print out languages
+			for(String language: theStudent.getLanguages()) {
+				System.out.println(language);
+			}
 			
 		}catch (Exception exc){
 			exc.printStackTrace();
